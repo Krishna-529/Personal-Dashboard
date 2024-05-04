50b12ac245c2bc92d0d39f7106f770c84ee8b2b2
Krishna Satyam <krishnasatyam50029@gmail.com>
2024-05-04 18:29:41 +0530

    homepage done

---
+++ script.js
@@ -0,0 +1,4 @@
+// const hide = function(element){
+//     element.style.display = "none";
+// }
+// document.getElementsByClassName("navbar")[0].addEventListener("click", function(){hide(document.getElementsByClassName("navbar")[0])});
